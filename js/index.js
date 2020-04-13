bed0c95d9a822b5e782b8ed0f6ef331a08ebb061
DmytroFitsa <dmytrofitsa@gmail.com>
2020-04-13 11:43:00 +0000

    ajaxfetch

--- js/index.js
+++ js/index.js
@@ -18,7 +18,32 @@ document.querySelector('.fetch-html').addEventListener('click', fetchhtml);
 //     .then(response => response.text() )
 //     .then(html => document.querySelector('.html-container').innerHTML = html);
 // }
-//======сучасний синтаксис asing away
-function fetchhtml() {
-    
+//======сучасний синтаксис async await
+async function fetchhtml() {
+    const response = await fetch('client-data.html');
+    const html = await response.text();
+    document.querySelector('.html-container').innerHTML = html
+}
+
+//with JSON
+document.querySelector('.get-ajax-json').addEventListener('click', getAjaxJSON);
+function getAjaxJSON() {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            const data = JSON.parse(xhr.responseText);
+            document.querySelector('.name').innerText = data.name;
+            document.querySelector('.balance').innerText = data.balance;
+        }
+    }
+    xhr.open('get', 'client-data.json', true);
+    xhr.send();
+}
+//=======
+document.querySelector('.fetch-json').addEventListener('click', fetchjson);
+async function fetchjson() {
+    const response = await fetch('client-data.json');
+    const data = await response.json();
+    document.querySelector('.name').innerText = data.name;
+    document.querySelector('.balance').innerText = data.balance;
 }
